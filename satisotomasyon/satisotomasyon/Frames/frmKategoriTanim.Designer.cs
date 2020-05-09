8aa6d89e84d931f9f91e2cc9e58b8fb7f19494f9
Oğulcan Özcan <ogulcan.ozcan@yalin.com.tr>
2020-05-10 01:07:39 +0300

    Ribbon control eklendi
    frames klasörü oluşturuldu

--- satisotomasyon/satisotomasyon/Frames/frmKategoriTanim.Designer.cs
+++ satisotomasyon/satisotomasyon/Frames/frmKategoriTanim.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace satisotomasyon
+﻿namespace satisotomasyon.Frames
 {
-    partial class Form1
+    partial class frmKategoriTanim
     {
         /// <summary>
         /// Required designer variable.
@@ -28,21 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // frmKategoriTanim
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ClientSize = new System.Drawing.Size(575, 513);
+            this.Controls.Add(this.button1);
+            this.Name = "frmKategoriTanim";
+            this.Text = "frmKategoriTanim";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
-
---
+++ satisotomasyon/satisotomasyon/frmMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+using satisotomasyon.Frames;
+
+namespace satisotomasyon
+{
+    public partial class frmMain : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKategoriTanimlari_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            frmKategoriTanim frm = new frmKategoriTanim();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+    }
+}
